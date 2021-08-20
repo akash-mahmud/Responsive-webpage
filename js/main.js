2838fe6333e4217d79ecdd61f843f3acc9d04f90
Akash Mahmud <87032071+akash-mahmud@users.noreply.github.com>
2021-08-20 22:22:03 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Bix slider
+
+$(document).ready(function(){
+  $('.slider_area').bxSlider();
+  jQuery('#main_menu').meanmenu({
+    meanMenuContainer: '#mobile_menu',
+    meanScreenWidth: 991,
+   
+  });
+});
